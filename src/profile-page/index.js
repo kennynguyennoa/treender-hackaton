d6ac77edae41733e1710a5f5adbd610faae9c4a1
Nicolai Rønning <97887844+nicolainoaignite@users.noreply.github.com>
2022-03-25 17:34:04 +0100

    Init

---
+++ src/profile-page/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const ProfilePage = () => {
+  return(
+    <div>
+      <div className="imgContainer">
+        <img alt="" src=""/>
+      </div>
+      <div className="textContainer"></div>
+    </div>
+    
+  );
+}
